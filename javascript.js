b9d7dda7a1a27468f240221660dc1df31085ca2a
Maisie Yan <maisiey78@gmail.com>
2024-06-29 17:44:38 -0400

    created global variables for scores and function for human choice and playing rounds

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3 + 1)
     switch(rand) {
@@ -12,4 +15,53 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let choice = prompt("What is your choice (rock, paper, or scissors)?")
+    return choice
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("You tied!")
+        }
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors!")
+            humanScore++
+        }
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats rock!")
+        }
+    }
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats rock!")
+            humanScore++
+        }
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats paper!")
+            computerScore++
+        }
+        if (computerChoice === "paper") {
+            console.log("You tie!")
+        }
+    }
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors!")
+            computerScore++
+        }
+        if (computerChoice === "scissors") {
+            console.log("You tie!")
+        }
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors beat paper!")
+            humanScore++
+        }
+    }
+
+}
+
+console.log(getHumanChoice())
 console.log(getComputerChoice())
