8eefdbffa0768c3f9a366482e913140a78600210
Maisie Yan <maisiey78@gmail.com>
2024-07-02 14:25:18 -0400

    added styling

--- javascript.js
+++ javascript.js
@@ -23,6 +23,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const resetBtn = document.querySelector('.reset');
     let gameOver = false;
+    let subtitle = document.querySelector('.subtitle');
+    const originalSubtitle = subtitle.textContent;
 
 
     let humanScore = 0;
@@ -61,14 +63,17 @@ document.addEventListener('DOMContentLoaded', () => {
         if (roundCount >= 5) {
             if (humanScore > computerScore) {
                 finalResult.textContent = "You win! Your score was " + humanScore + " and the computer score was " + computerScore + ".";
+                finalResult.style.backgroundColor = 'green';
             }
             
             if (humanScore < computerScore) {
                 finalResult.textContent = "You lose. Your score was " + humanScore + " and the computer score was " + computerScore + ".";
+                finalResult.style.backgroundColor = 'red';
             }
             
             if (humanScore === computerScore) {
                 finalResult.textContent = "You tied. Your score was " + humanScore + " and the computer score was " + computerScore + ".";
+                finalResult.style.backgroundColor = 'grey';
             }
             roundResult.textContent = '';
             gameOver = true;
@@ -82,12 +87,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         roundResult.textContent = '';
         finalResult.textContent = '';
+        finalResult.style.backgroundColor = 'white';
         
+        subtitle.textContent = originalSubtitle;
         gameOver = false;
     }
 
     selection.addEventListener('click', (event) => {
         let target = event.target;
+        subtitle.textContent = '';
 
         if (target.classList.contains('rock')) {
             playRound('rock', getComputerChoice());
